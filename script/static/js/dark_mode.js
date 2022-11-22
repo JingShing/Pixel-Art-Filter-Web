28aee659c30a68b172965673467de04f0e110761
JingShing <JingShingLai@gmail.com>
2022-11-22 23:56:38 +0800

    Add files via upload

---
+++ script/static/js/dark_mode.js
@@ -0,0 +1,18 @@
+var options = {
+    bottom: '32px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#0d0d0d',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: false // default: true
+};
+function init(){
+    const darkmode = new Darkmode(options);
+    darkmode.showWidget()
+}
+window.addEventListener('load', init, false);
