f62ba18393722634d8af924a47246ff2f8c5fe58
JingShing <JingShingLai@gmail.com>
2022-11-24 21:39:57 +0800

    Add files via upload

--- script/pixel_page.py
+++ script/pixel_page.py
@@ -46,14 +46,19 @@ def callback():
     auth = tweepy.OAuthHandler(api_key, api_secret)
     auth.request_token = {'oauth_token': oauth_token, 'oauth_token_secret': oauth_verifier}
     auth.get_access_token(oauth_verifier)
-    user_token = auth.access_token
-    user_token_secret = auth.access_token_secret
+    user_tokens = f"access-token={auth.access_token}<br>access-token-secret={auth.access_token_secret}"
+    return user_tokens
+@app.route('/tweet')
+def tweet():
+    # You would read these values from the session
+    user_token = os.getenv('access-token')
+    user_token_secret = os.getenv('access-token-secret')
+    auth = tweepy.OAuthHandler(api_key, api_secret)
     auth.set_access_token(user_token,user_token_secret)
     api = tweepy.API(auth)
     # Create a tweet - random() to not write same tweet twice
     api.update_status(f"A Tweet from Flask - {random.random()}")
-    # user_token = f"access-token={auth.access_token}<br>access-token-secret={auth.access_token_secret}"
-    # return user_token
+ 
     return "Tweet send with Flask"
 
 def around_value(value, min_num, max_num):
