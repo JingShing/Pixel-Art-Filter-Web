4f57a54f02da753d0c318e549a74229d9eed0c31
JingShing <JingShingLai@gmail.com>
2022-11-25 10:56:00 +0800

    Update qrcode_process.py

--- script/qrcode_process.py
+++ script/qrcode_process.py
@@ -4,7 +4,7 @@ def qr_code_process(src_path, content):
     result_paths = src_path.split('.')
     if result_paths[-1].lower()=='jpg':result_paths[-1]='png'
     result_path = result_paths[0] + '_qrcode.' + result_paths[-1]
-    result_path = src_path.split('.')[0] + '_qrcode.' + src_path.split('.')[-1]
+    # result_path = src_path.split('.')[0] + '_qrcode.' + src_path.split('.')[-1]
     myqr.run(words = content,
          picture = src_path,
          version=10, # this is length
@@ -13,4 +13,4 @@ def qr_code_process(src_path, content):
          save_name = result_path)
     return result_path
 
-# qr_code_process(src_path='static/results/test.png', content='test')
+# qr_code_process(src_path='static/results/test.jpg', content='test')
