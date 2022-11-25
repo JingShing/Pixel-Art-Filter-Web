e9c4434013a9c0146dc1ff033dec69a11ed72411
JingShing <JingShingLai@gmail.com>
2022-11-25 10:53:45 +0800

    Update qrcode_process.py

--- script/qrcode_process.py
+++ script/qrcode_process.py
@@ -1,10 +1,10 @@
 from MyQR import myqr
 
 def qr_code_process(src_path, content):
-#     result_paths = src_path.split('.')
-#     if (result_paths[-1].lower()=='jpg')result_paths[-1]='png'
-#     result_path = result_paths[0] + '_qrcode.' + result_paths[-1]
-    result_path = src_path.split('.')[0] + '_qrcode.' + result_paths[-1]
+    result_paths = src_path.split('.')
+    if result_paths[-1].lower()=='jpg':result_paths[-1]='png'
+    result_path = result_paths[0] + '_qrcode.' + result_paths[-1]
+    result_path = src_path.split('.')[0] + '_qrcode.' + src_path.split('.')[-1]
     myqr.run(words = content,
          picture = src_path,
          version=10, # this is length
