b35faf65fa59ef82e1bc6c64838514f9c75e6987
JingShing <JingShingLai@gmail.com>
2022-11-26 10:00:44 +0800

    Add files via upload

--- script/static/js/pixel_set.js
+++ script/static/js/pixel_set.js
@@ -1,10 +1,10 @@
 const pixel_set_vars = ["k","scale","blur","erode","saturation","contrast","qr_code_content", "original_img_src", "result_img_src"]
-const session_set = ["user_key", "user_secret_key", "user_key2", "user_secret_key2"]
+// const session_set = ["user_key", "user_secret_key", "user_key2", "user_secret_key2"]
 const pixel_checkbox = ["alpha", "to_tw", "qr_code"]
 function save_pixel_set_to_local_storage(){
     for(let i = 0;i<pixel_set_vars.length;i++)localStorage.setItem(pixel_set_vars[i], document.getElementById(pixel_set_vars[i]).value);
     for(let i = 0;i<pixel_checkbox.length;i++)localStorage.setItem(pixel_checkbox[i], document.getElementById(pixel_checkbox[i]).checked);
-    for(let i = 0;i<session_set.length;i++)sessionStorage.setItem(session_set[i], document.getElementById(session_set[i]).value);
+    // for(let i = 0;i<session_set.length;i++)sessionStorage.setItem(session_set[i], document.getElementById(session_set[i]).value);
 }
 function load_pixel_set_to_local_storage(){
     for(let i = 0;i<pixel_set_vars.length;i++)if(localStorage.getItem(pixel_set_vars[i]))document.getElementById(pixel_set_vars[i]).value=localStorage.getItem(pixel_set_vars[i])
@@ -13,7 +13,7 @@ function load_pixel_set_to_local_storage(){
         else document.getElementById(pixel_checkbox[i]).removeAttribute("checked");
     }
     qr_code_enable();
-    for(let i = 0;i<session_set.length;i++)document.getElementById(session_set[i]).value = sessionStorage.getItem(session_set[i]);
+    // for(let i = 0;i<session_set.length;i++)document.getElementById(session_set[i]).value = sessionStorage.getItem(session_set[i]);
 }
 function qr_code_enable(){
     if(document.getElementById('qr_code').checked==true){
