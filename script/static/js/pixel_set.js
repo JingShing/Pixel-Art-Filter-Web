087eb75f46004a6865ddaa5eb27b5cb1d6656153
JingShing <JingShingLai@gmail.com>
2022-11-26 15:02:11 +0800

    Add files via upload

--- script/static/js/pixel_set.js
+++ script/static/js/pixel_set.js
@@ -4,7 +4,8 @@ const pixel_checkbox = ["alpha", "to_tw", "qr_code"]
 function save_pixel_set_to_local_storage(){
     for(let i = 0;i<pixel_set_vars.length;i++)localStorage.setItem(pixel_set_vars[i], document.getElementById(pixel_set_vars[i]).value);
     for(let i = 0;i<pixel_checkbox.length;i++)localStorage.setItem(pixel_checkbox[i], document.getElementById(pixel_checkbox[i]).checked);
-    for(let i = 0;i<session_set.length;i++)sessionStorage.setItem(session_set[i], document.getElementById(session_set[i]).value);
+    // for(let i = 0;i<session_set.length;i++)sessionStorage.setItem(session_set[i], document.getElementById(session_set[i]).value);
+    for(let i = 0;i<session_set.length;i++)localStorage.setItem(session_set[i], document.getElementById(session_set[i]).value);
 }
 function load_pixel_set_to_local_storage(){
     for(let i = 0;i<pixel_set_vars.length;i++)if(localStorage.getItem(pixel_set_vars[i]))document.getElementById(pixel_set_vars[i]).value=localStorage.getItem(pixel_set_vars[i])
@@ -13,7 +14,8 @@ function load_pixel_set_to_local_storage(){
         else document.getElementById(pixel_checkbox[i]).removeAttribute("checked");
     }
     qr_code_enable();
-    for(let i = 0;i<session_set.length;i++)document.getElementById(session_set[i]).value = sessionStorage.getItem(session_set[i]);
+    // for(let i = 0;i<session_set.length;i++)document.getElementById(session_set[i]).value = sessionStorage.getItem(session_set[i]);
+    for(let i = 0;i<session_set.length;i++)document.getElementById(session_set[i]).value = localStorage.getItem(session_set[i]);
 }
 function qr_code_enable(){
     if(document.getElementById('qr_code').checked==true){
